6691aa6e4d91e953db1bfb44e58863e7bf3f25fa
MSI <MSI@192.168.1.35>
2024-12-06 11:30:49 +0530

    change in controller

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 	 @GetMapping("/")
 	    public String sayHello() {
-	        return "Hello, World!";
+	        return "Hello,how are you!";
 	    }
 }
